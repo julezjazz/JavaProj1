b53d1f3bbf8be93f9414b8a237c26491e704cb05
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-19 14:00:24 -0400

    Removed idea folder from tracking

--- src/model/InHouse.java
+++ src/model/InHouse.java
@@ -1,2 +1,20 @@
-package model;public class InHouse {
+package model;
+
+public class InHouse extends Part {
+
+    private int machineId;
+
+    public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
+        super(id, name, price, stock, min, max);
+        this.machineId = machineId;
+    }
+
+    public void setMachineId(int machineId) {
+        this.machineId = machineId;
+        //is this okay even though we've already used a parameter named machineId to set this?
+    }
+
+    public int getMachineId() {
+        return machineId;
+    }
 }
