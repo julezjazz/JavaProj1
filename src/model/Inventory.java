144d399d138769dd4db5e7346eabebf30e86b6ac
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-22 11:49:08 -0400

    Added commented-out skeleton of all remaining methods for Inventory class

--- src/model/Inventory.java
+++ src/model/Inventory.java
@@ -6,12 +6,35 @@ import javafx.collections.ObservableList;
 public class Inventory {
 
     private static ObservableList<Part> allParts = FXCollections.observableArrayList();
+    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
 
-    public static void addPart(Part newPart) {
-        allParts.add(newPart);
-    }
+    //parts
+    public static void addPart(Part newPart) {allParts.add(newPart);}
 
-    public static ObservableList<Part> getAllParts() {
-        return allParts;
-    }
+    //public static Part lookupPart(int partId){}
+
+    //public static ObservableList<Part> lookupPart(String partName){}
+
+    //public static void updatePart(int index, Part selectedPart){}
+
+    //public static boolean deletePart(Part selectedPart){allParts.remove(selectedPart);}
+    //how do I make above boolean?
+
+    public static ObservableList<Part> getAllParts() {return allParts;}
+
+
+   //products
+    public static void addProduct(Product newProduct) {allProducts.add(newProduct);}
+
+    //public static Product lookupProduct(int productId){}
+
+    //public static ObservableList<Product> lookupProduct(String productName){}
+
+    //public static void updateProduct(int index, Product newProduct){}
+
+    //public static boolean deleteProduct(Product selectedProduct){allProducts.remove(selectedProduct);}
+    //how do I make above boolean?
+
+    public static ObservableList<Product> getAllProducts() {return allProducts;}
+
 }
