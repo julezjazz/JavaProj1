002def6b32710bdb872e3747685694c283e81b0b
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-23 16:46:43 -0400

    Added test data to Inventory

--- src/model/Inventory.java
+++ src/model/Inventory.java
@@ -37,4 +37,28 @@ public class Inventory {
 
     public static ObservableList<Product> getAllProducts() {return allProducts;}
 
+    //Test Data Below
+    static {
+       addTestData();
+    }
+
+    public static void addTestData(){
+        InHouse inHouse1 = new InHouse(1,"Part One", 4,3, 1, 4, 123);
+        InHouse inHouse2 = new InHouse(2, "Part Two", 6, 4, 1, 5, 321);
+        Outsourced outsourced1 = new Outsourced(3, "Part 3", 14, 2, 2, 5, "MT");
+        Outsourced outsourced2 = new Outsourced(4, "Part 4", 16, 3, 2, 5, "TM");
+
+        allParts.add(inHouse1);
+        allParts.add(inHouse2);
+        allParts.add(outsourced1);
+        allParts.add(outsourced2);
+
+        Product product1 = new Product(11, "Product One", 22, 2, 1, 4);
+        Product product2 = new Product(12, "Product 2", 17, 1, 1, 5);
+
+        allProducts.add(product1);
+        allProducts.add(product2);
+
+    }
+
 }
