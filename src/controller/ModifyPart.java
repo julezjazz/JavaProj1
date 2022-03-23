d127f5b987b4719a4d8d081fa1fca911d0d2becd
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-23 10:30:40 -0400

    Made each controller class implement Initializable

--- src/controller/ModifyPart.java
+++ src/controller/ModifyPart.java
@@ -1,4 +1,13 @@
 package controller;
 
-public class ModifyPart {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModifyPart implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
