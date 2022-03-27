e27f3efadd8c1c586a5d5af12811d1bf78321fd6
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-27 15:59:34 -0400

    Added textfields and empty onSaveButton method for AddPart

--- src/controller/AddPart.java
+++ src/controller/AddPart.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +14,17 @@ public class AddPart implements Initializable {
     public Label toggleLabel;
     public RadioButton inHouse;
     public RadioButton outsourced;
+    // Unsure about these text fields 3/27/22
+    public TextField nameTF;
+    public TextField stockTF;
+    public TextField priceTF;
+    public TextField maxTF;
+    public TextField minTF;
+    public TextField toggleTF;
+
+
+
+
 
 
     @Override
@@ -28,4 +40,11 @@ public class AddPart implements Initializable {
     public void onInHouse(ActionEvent actionEvent) {
         toggleLabel.setText("Machine ID");
     }
+// Unsure about this 3/27/22
+    public void onSaveButton(ActionEvent actionEvent) {
+
+    }
+
+
+
 }
