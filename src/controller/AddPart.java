0109e94b9e54e7b7f0cb3207afbcc5cdf6a71077
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-28 12:12:33 -0400

    Added functionality to cancel button

--- src/controller/AddPart.java
+++ src/controller/AddPart.java
@@ -1,12 +1,18 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.InHouse;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -51,5 +57,12 @@ public class AddPart implements Initializable {
     }
 
 
-
+    public void onCancelButton(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/MainScreen.fxml"));
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root, 600, 400);
+        stage.setTitle("Modify Product");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
