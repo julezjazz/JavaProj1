bd505b7b82f8777c8cbd771090f821d8be4bafbd
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-28 13:31:39 -0400

    Linked first table with data and made search bar functional

--- src/controller/AddProduct.java
+++ src/controller/AddProduct.java
@@ -1,21 +1,72 @@
 package controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Inventory;
+import model.Part;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddProduct implements Initializable {
+
+    public TableView partsTable2;
+
+    public TableColumn partIdCol2;
+    public TableColumn partNameCol2;
+    public TableColumn partInvLevelCol2;
+    public TableColumn partPriceCol2;
+
+    public TextField searchBarPart2;
+
+    public Label warningLabel;
+
+    public Inventory inventory;
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        partsTable2.setItems(inventory.getAllParts());
 
+        partIdCol2.setCellValueFactory(new PropertyValueFactory<>("id"));
+        partNameCol2.setCellValueFactory(new PropertyValueFactory<>("name"));
+        partInvLevelCol2.setCellValueFactory(new PropertyValueFactory<>("stock"));
+        partPriceCol2.setCellValueFactory(new PropertyValueFactory<>("price"));
+    }
+
+    public void getResultsParts2(ActionEvent actionEvent) {
+        warningLabel.setText("");
+
+        String s = searchBarPart2.getText();
+
+        ObservableList<Part> parts = inventory.lookupPart(s);
+
+        partsTable2.setItems(parts);
+        searchBarPart2.setText("");
+
+        if (parts.size() == 0) {
+
+            int id = Integer.parseInt(s);
+            Part part = inventory.lookupPart(id);
+            if (part != null)
+                parts.add(part);
+        }
+        if (parts.size() == 0) {
+            warningLabel.setText("Part could not be found. Please try a new search.");
+        }
     }
 
     public void onCancelButton(ActionEvent actionEvent) throws IOException {
@@ -27,3 +78,5 @@ public class AddProduct implements Initializable {
         stage.show();
     }
 }
+
+
