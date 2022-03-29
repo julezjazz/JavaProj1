f9cc78ceba42c51fcf21e10b6df91f5e45090926
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-29 08:17:31 -0400

    Gave functionality to save buttin in addPart

--- src/controller/AddPart.java
+++ src/controller/AddPart.java
@@ -11,6 +11,7 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import model.InHouse;
+import model.Inventory;
 
 import java.io.IOException;
 import java.net.URL;
@@ -21,13 +22,15 @@ public class AddPart implements Initializable {
     public Label toggleLabel;
     public RadioButton inHouse;
     public RadioButton outsourced;
-    // Unsure about these text fields 3/27/22
+
     public TextField nameTF;
     public TextField stockTF;
     public TextField priceTF;
     public TextField maxTF;
     public TextField minTF;
     public TextField toggleTF;
+
+    public Inventory inventory;
 
 
 
@@ -47,10 +50,22 @@ public class AddPart implements Initializable {
     public void onInHouse(ActionEvent actionEvent) {
         toggleLabel.setText("Machine ID");
     }
-// Unsure about this 3/27/22
-    public void onSaveButton(ActionEvent actionEvent) {
-        if(inHouse.isSelected()) {
 
+
+    public void onSaveButton(ActionEvent actionEvent) throws IOException {
+        if(inHouse.isSelected()) {
+          InHouse iH = new InHouse(1, nameTF.getText(), Double.parseDouble(priceTF.getText()),
+                  Integer.parseInt(stockTF.getText()), Integer.parseInt(minTF.getText()),
+                  Integer.parseInt(maxTF.getText()), Integer.parseInt(toggleTF.getText()));
+
+          inventory.addPart(iH);
+
+            Parent root = FXMLLoader.load(getClass().getResource("../view/MainScreen.fxml"));
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root, 850, 750);
+            stage.setTitle("Inventory Management");
+            stage.setScene(scene);
+            stage.show();
 
         }
 
