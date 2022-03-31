d971750b17b2badfdbd460ac08bcfdc8bb153699
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-31 17:58:18 -0400

    Added javadoc comments to class and methods

--- src/model/Outsourced.java
+++ src/model/Outsourced.java
@@ -1,18 +1,18 @@
 package model;
-
+/** Outsourced is derived from Part. */
 public class Outsourced extends Part{
 
     private String companyName;
-
+    /** This is the constructor for Outsourced objects. */
     public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName) {
         super(id, name, price, stock, min, max);
         this.companyName = companyName;
     }
-
+    /** This method sets the company name for Outsourced objects. */
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
-
+    /** This returns the company name for an Outsourced object. */
     public String getCompanyName() {
         return companyName;
     }
