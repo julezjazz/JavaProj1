c6b6ba7b34468bb233a3d64e87ab6a2036bfba65
julezjazz <55053987+julezjazz@users.noreply.github.com>
2022-03-31 17:49:45 -0400

    Added javadoc comments to class and methods

--- src/model/InHouse.java
+++ src/model/InHouse.java
@@ -1,18 +1,18 @@
 package model;
-
+/** InHouse is derived from Part. */
 public class InHouse extends Part {
 
     private int machineId;
-
+    /** This is the constructor for InHouse objects. */
     public InHouse(int id, String name, double price, int stock, int min, int max, int machineId) {
         super(id, name, price, stock, min, max);
         this.machineId = machineId;
     }
-
+    /** This sets the Machine ID for InHouse objects. */
     public void setMachineId(int machineId) {
         this.machineId = machineId;
     }
-
+    /** This returns the machine ID for InHouse objects. */
     public int getMachineId() {
         return machineId;
     }
